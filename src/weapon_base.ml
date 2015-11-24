c86cec8c2952c77e66e6295a9d9c057b82d81aa8
derui <derutakayu@gmail.com>
2015-11-24 22:31:36 +0900

    Add weapon base type.

---
+++ src/weapon_base.ml
@@ -0,0 +1,14 @@
+open Core.Std
+
+type component = Region_base.region_type * int [@@deriving sexp]
+(* A component of the weapon. Having component region type and count are attachable to a
+   weapon.
+*)
+
+(* The type of weapon *)
+type t = {
+  regions: Region_base.region list;
+  (* Regions constructed a weapon *)
+  components: component list;
+(* Components defines attachable region type and count. *)
+}
