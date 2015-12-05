b2627d8fbe787d448a1dd40bff8a809cfcd3ac40
derui <derutakayu@gmail.com>
2015-12-05 17:58:44 +0900

    Change signature for loading setting

--- src/ability_setting.ml
+++ src/ability_setting.ml
@@ -8,12 +8,16 @@ type t = {
   (* A list of mergeable ability *)
 } [@@deriving sexp]
 
+let empty = {mergeable_abilities = []; unmergeable_abilities = []}
+
 let save setting file =
   Out_channel.with_file file ~f:(fun f ->
     ([%sexp_of:t] setting) |> Sexp.to_string |> Out_channel.output_string f 
   )
 
 let load file =
-  In_channel.with_file file ~f:(fun f ->
+  match Sys.file_exists file with
+  | `Yes -> Option.some (In_channel.with_file file ~f:(fun f ->
     Sexp.input_sexp f |> [%of_sexp:t]
-  )
+  ))
+  | _ -> None
--- test/ability_setting_test.ml
+++ test/ability_setting_test.ml
@@ -7,7 +7,7 @@ let%spec "Ability setting can write to and load from a file" =
     unmergeable_abilities = [`Slash_defence]
   } in
   S.save setting "sample.sexp";
-  let loaded = S.load "sample.sexp" in
+  let loaded = S.load "sample.sexp" |> Option.value ~default:S.empty in
 
   loaded.S.mergeable_abilities [@eq [`Slash_attack]];
   loaded.S.unmergeable_abilities [@eq [`Slash_defence]]
